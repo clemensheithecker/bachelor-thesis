f31aa6e792738893fb98eddb961b84ffb7a0be72
Clemens S. Heithecker <clemens@heithecker.dev>
2022-04-24 17:53:20 +0200

    Add missing value exploration

--- scripts/data-cleaning.R
+++ scripts/data-cleaning.R
@@ -13,7 +13,16 @@ library(haven)
 # Manipulate metadata as variable and value labels
 library(labelled)
 
+# A graphic device which supports custom fonts
+library(ragg)
+
 library(tidyverse)
+
+
+# Source scripts ----------------------------------------------------------
+
+# A script that creates two custom ggplot2 themes
+source("ggplot2-themes.R")
 
 
 # Select variables of interest --------------------------------------------
@@ -58,6 +67,113 @@ gss_raw <- gss_raw %>%
   mutate_if(is.labelled, ~ to_factor(., levels = "l"))
 
 glimpse(gss_raw)
+
+
+# Understanding missing values --------------------------------------------
+
+missing_values_summary <- gss_raw %>%
+  group_by(year) %>%
+  summarise(
+    across(everything(), ~ sum(is.na(.x))),
+    n = n()
+  ) %>%
+  ungroup() %>%
+  # Divide all columns but `year` and `n` by `n`
+  mutate(across(-c(year, n), ~ .x / n)) %>%
+  # Add "_fraction" suffix to all columns but `year` and `n`
+  rename_with(~ paste0(., "_fraction_na"), - c(year, n))
+
+head(missing_values_summary)
+glimpse(missing_values_summary)
+
+
+missing_values_summary_long <- missing_values_summary %>%
+  select(-n) %>%
+  rename_all( ~ stringr::str_replace(., "_fraction_na", "")) %>%
+  pivot_longer(!year, names_to = "variable", values_to = "fraction_missing") %>%
+  mutate(fraction_present = 1 - fraction_missing) %>%
+  pivot_longer(
+    !c(year, variable),
+    names_to = "measure",
+    values_to = "fraction"
+  )
+
+
+missing_values_summary_plot <- function(df, filename, title) {
+  plot <- df %>%
+    ggplot(mapping = aes(x = fraction, y = variable)) +
+    geom_col(
+      mapping = aes(fill = measure),
+      position = position_stack(reverse = TRUE)
+    ) +
+    geom_text(
+      mapping = aes(label = ifelse(measure == "fraction_missing",
+                                   scales::percent(fraction, accuracy = 0.1),
+                                   ""),
+                    hjust = ifelse(measure == "fraction_missing" &
+                                     fraction > 0.7,
+                                   1.125,
+                                   -0.125),
+                    color = ifelse(measure == "fraction_missing" &
+                                     fraction > 0.7,
+                                   "white",
+                                   "black")
+      ),
+    ) +
+    facet_wrap(~ year, ncol = 4) +
+    scale_y_discrete(limits = rev) +
+    scale_x_continuous(labels = scales::percent) +
+    scale_color_manual(values = c("white" = "#ffffff",
+                                  "black" = "#000000"),
+                       guide = "none") +
+    scale_fill_manual(labels = c("fraction_missing" = "Missing",
+                                 "fraction_present" = "Present"),
+                      values = c("fraction_missing" = "#262626",
+                                 "fraction_present" = "#E5E5E5")) +
+    labs(
+      x = "",
+      y = "",
+      title = title
+    ) +
+    theme_thesis() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.major.y = element_blank()
+    )
+  
+  ggsave(
+    filename = filename,
+    plot = plot,
+    device = agg_png,
+    res = 300,
+    width = 16 * 1.5,
+    height = 3 / 4 * 16 * 1.5,
+    units = "cm"
+  )
+}
+
+
+missing_values_summary_long %>%
+  filter(year <= 1985) %>%
+  missing_values_summary_plot(
+    filename = "../figures/missing-values-summary-plot-1.png",
+    title = "Fraction of Missing Values (1)"
+  )
+
+missing_values_summary_long %>%
+  filter(year > 1985 & year <= 2002) %>%
+  missing_values_summary_plot(
+    filename = "../figures/missing-values-summary-plot-2.png",
+    title = "Fraction of Missing Values (2)"
+  )
+
+missing_values_summary_long %>%
+  filter(year > 2002) %>%
+  missing_values_summary_plot(
+    filename = "../figures/missing-values-summary-plot-3.png",
+    title = "Fraction of Missing Values (3)"
+  )
 
 
 # Clean variables ---------------------------------------------------------
@@ -112,7 +228,7 @@ nrow(gss)
 # number of observations = 67895
 
 # Omit observations with missing values in all years but 2021
-# Note: The GSS was conduted online in 2021 thus data on race and region is not
+# Note: The GSS was conducted online in 2021 thus data on race and region is not
 # available. If I drop all records containing NA values, I would drop all 2021
 # records
 gss_pre_2021 <- gss %>%
